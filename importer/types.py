ae9c0221da2b8e07c5bed3f64657dffed1d9f954
Robert Purcell <coder-git@robertology.com>
2020-08-11 22:31:05 -0400

    get query from a definition

--- importer/types.py
+++ importer/types.py
@@ -8,11 +8,31 @@ class ImportType(ABC):
         super().__init__()
 
     @abstractmethod
-    def get_import_data():
+    def _get_import_data_keys(self):
         pass
 
+    def get_import_data(self):
+        return self._get_data_from_keys(self._get_import_data_keys(), self.data)
+
+    def _get_data_from_keys(self, keys, input_data):
+        data = {}
+
+        for key, val in keys.items():
+            value = input_data.get(key, None)
+            if value is not None:
+                if isinstance(val, dict):
+                    value = self._get_data_from_keys(val, value)
+
+                data[key] = value
+
+        return data
 
 class Category(ImportType):
-    def get_import_data(self):
-        # TODO
-        pass
+    def _get_import_data_keys(self):
+        # TODO: image upload
+        return {
+            "slug": "",
+            "name": "",
+            "description": "",
+            "seo": {"title": "", "description": ""},
+        }
